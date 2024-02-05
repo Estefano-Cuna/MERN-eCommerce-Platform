5a1dca6099decd719f1b0b3f5b3e99f162d844a0
Estefano Cuna <estefanocuna@yahoo.com>
2024-02-04 22:08:22 -0500

    Adds shopping cart functionality to the frontend

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
-import { Navbar, Nav, Container } from 'react-bootstrap';
+import { Badge, Navbar, Nav, Container } from 'react-bootstrap';
 import { FaShoppingCart, FaUser } from 'react-icons/fa';
 import { LinkContainer } from 'react-router-bootstrap';
+import { useSelector } from 'react-redux';
 import logo from '../assets/logo.png';
 import { Link } from 'react-router-dom';
 
 const Header = () => {
+  const { cartItems } = useSelector(state => state.cart);
+  
+    
   return (
     <header>
         <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
@@ -20,7 +24,14 @@ const Header = () => {
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="ms-auto">
                         <LinkContainer to='/cart'>
-                        <Nav.Link><FaShoppingCart /> Cart</Nav.Link>
+                        <Nav.Link>
+                            <FaShoppingCart /> Cart
+                            {cartItems.length > 0 && (
+                                <Badge pill bg ='success' style={{ marginLeft: '5px' }}>
+                                    {cartItems.reduce((a, c) => a + c.qty, 0)}
+                                </Badge>
+                            )}
+                            </Nav.Link>
                         </LinkContainer>
                         <LinkContainer to='/login'>
                         <Nav.Link href="/login"><FaUser /> Sign In</Nav.Link>
